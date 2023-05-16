6b31bd0623baee28ad32f3303e627ad4d819ffe2
Olena Zdanovska <freshjs.ftm@gmail.com>
2023-05-16 16:36:23 +0300

    Add Docker-compose

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
+// require('dotenv').config();
 const http = require('http');
 const mongoose = require('mongoose');
 const app = require('./app');
 
-mongoose.connect('mongodb://172.17.0.2:27017/fd_mongoose')
+const host = process.env.HOST || 'fd_mongo';
+
+mongoose.connect(`mongodb://${host}:27017/fd_mongoose`)
 .catch((error) => {
   console.log(error);
   process.exit(1);
